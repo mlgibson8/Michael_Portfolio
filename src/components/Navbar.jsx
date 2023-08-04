d18de82d3f2763c471c2fe65d7aea913a62b7b71
mlgibson8 <michael.l.gibson@du.edu>
2023-08-04 16:48:48 -0600

    setup clicker, menus

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FaBars, FaTimes} from 'react-icons/fa';
 import {ReactComponent as Logo} from '../assets/mainLogo.svg';
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
  <div >
 <Logo style={{width: '85px'}} />
  </div>
 {/* menu */}
- <div>
-    <ul className='flex'>
+
+    <ul className='hidden md:flex'>
         <li>Home</li>
         <li>About</li>
         <li>Portfolio</li>
         <li>Contact</li>
         <li>Resume</li>
     </ul>
- </div>
+
 {/* menu2 */}
-<div className='hidden'><FaBars />
+<div className='md:hidden z-10'><FaBars />
 </div>
 {/* mobile menu */}
-<ul className='hidden'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
-        <li>Resume</li>
+<ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center  '>
+        <li className='py-6 text-4xl'>Home</li>
+        <li className='py-6 text-4xl'>About</li>
+        <li className='py-6 text-4xl'>Portfolio</li>
+        <li className='py-6 text-4xl'>Contact</li>
+        <li className='py-6 text-4xl'>Resume</li>
     </ul>
 
 {/* socials */}
