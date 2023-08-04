7524e9139b98d0e7a9226633227449979ff7a1a7
mlgibson8 <michael.l.gibson@du.edu>
2023-08-04 16:22:20 -0600

    setup logo svg, nav menu items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa';
+import {ReactComponent as Logo} from '../assets/mainLogo.svg';
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'>
-        
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+ <div >
+<Logo style={{width: '85px'}} />
+ </div>
+{/* menu */}
+ <div>
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+        <li>Resume</li>
+    </ul>
+ </div>
     </div>
   )
 }
