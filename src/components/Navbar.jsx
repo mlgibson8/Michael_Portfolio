5d615be207d33fc53c951800f77702c9a5bdb6b5
mlgibson8 <michael.l.gibson@du.edu>
2023-08-04 16:54:28 -0600

    changedstates of menu for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,10 +21,11 @@ const Navbar = () => {
     </ul>
 
 {/* menu2 */}
-<div className='md:hidden z-10'><FaBars />
+<div onClick={handleClick} className='md:hidden z-10'>
+    {!nav ? <FaBars/> : <FaTimes/>}
 </div>
 {/* mobile menu */}
-<ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center  '>
+<ul className={!nav ? 'hidden':'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
         <li className='py-6 text-4xl'>Home</li>
         <li className='py-6 text-4xl'>About</li>
         <li className='py-6 text-4xl'>Portfolio</li>
