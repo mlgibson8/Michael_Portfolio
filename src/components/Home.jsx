76b365d3f98a247a0bc2bd303cdb77a9386997b8
mlgibson8 <michael.l.gibson@du.edu>
2023-08-04 18:09:32 -0600

    finished home starting about

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,13 +5,17 @@ const Home = () => {
 
     <div name="home" className='w-full h-screen bg-[#0a192f]'>
     {/* container */}
-    <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full bg-slate-50'>
+    <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
       <p className='text-red-400'>Hello! Welcome to my page.</p>
       <h1 className='text-4xl sm:text-7xl font-bold text-teal-600'>I'm Michael</h1>
-      <h2 className='text-4xl sm:text-7xl font bold text-blue-300'> a full-stack developer!</h2>
-      <p>A recent graduate of Denver University</p>
+      <h2 className='text-4xl sm:text-7xl font bold text-green-500'> a full-stack developer!</h2>
+      <p className='text-blue-500 py-4 max-w-[700px]'>A recent graduate of Denver University blah blah</p>
       <div>
-        <button>work <HiArrowNarrowRight /></button>
+        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-teal-600 hover:border-pink-600'>work 
+        <span className='group-hover:rotate-90 duraton-300'>
+        <HiArrowNarrowRight className='ml-3' />
+        </span>
+        </button>
       </div>
     </div>
     </div>
