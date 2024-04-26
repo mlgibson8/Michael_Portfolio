81745792ebaf703f7aa3863d3ed790e25becc5c5
mlgibson8 <michael.l.gibson@du.edu>
2024-04-26 15:31:25 -0600

    fixing speed testing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
+
 function App() {
   return (
     <div >
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
